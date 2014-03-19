2bea857bf1733896e4267ad3b2dc6c8796d0e112
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2014-03-19 14:55:59 -0400

    updated setup a little bit

--- setup.py
+++ setup.py
@@ -2,24 +2,40 @@
 # Copyright 2013 Shannon Cruey
 #########################################################################
 
-# NOT AT ALL READY YET
+# TRICKY TO do this using setup.py - since we require external directories,
+# and the code currently expects those directories to have a path relative
+# to the executable.
+
+# First, restructure the code so the package is installed in site-packages,
+# the config file goes in /etc/flexapi.conf, and the conf file tells where
+# the other directories go (defaulted to /opt/flexapi/*)
+
 
 # import os
-# import setuptools
+# from setuptools import setup, find_packages
 # 
-# binscripts = []
+# def read(*paths):
+#     """Build a file path from *paths* and return the contents."""
+#     with open(os.path.join(*paths), 'r') as f:
+#         return f.read()
+# 
+#  
+# binscripts = [
+#               "/bin/flexapi"
+#               ]
 # for f in os.listdir("bin"):
 #     binscripts.append("bin/"+f)
-# 
-# setuptools.setup(
+#  
+# setup(
 #     name='FlexAPI',
-#     version='0.1',
-#     description='A flexible, customizable REST API builder.',
+#     version='0.1.0',
+#     description=read('README.rst'),
 #     license='Proprietary License',
 #     author='Shannon Cruey',
 #     author_email='',
 #     url='http://flexapi.info',
-#     packages=setuptools.find_packages(exclude=[]),
+#     install_requires=['web.py'],
+#     packages=find_packages(exclude=[]),
 #     include_package_data=True,
 #     classifiers=[
 #         'Development Status :: Beta',
